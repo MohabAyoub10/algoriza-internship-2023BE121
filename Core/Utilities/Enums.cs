923b2b156d9c60bac06694f22cf2a83f60a59a57
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-04 18:30:00 +0200

    .

---
+++ Core/Utilities/Enums.cs
@@ -0,0 +1,44 @@
+﻿namespace Core.Utilities
+{
+    public enum Day
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+
+    public enum BookingStatus
+    {
+        Pending,
+        Completed,
+        Cancelled
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public enum DiscountType
+    {
+        Percentage,
+        Amount
+    }
+
+    public enum UserType
+    {
+        Patient,
+        Admin,
+        Doctor
+    }
+    public enum ActivationStatus
+    {
+        Active,
+        Inactive
+    }
+}
