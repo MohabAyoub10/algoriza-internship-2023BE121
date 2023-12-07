ae25ce8d8f6f39ffad2b718f49ddf9715043abdd
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-07 20:27:25 +0200

    Created DTOs To Create and Update the Doctor Schedule and their time slots

---
+++ Core/DTO/AppointmentsDTO.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class AppointmentsDTO
+    {
+        public int Id { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public List<string> times { get; set; }
+        public ActivationStatus AppointmentStatus { get; set; }
+    }
+
+    
+}
---
+++ Core/DTO/DoctorScheduleDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities;
+
+namespace Core.DTO
+{
+    public class DoctorScheduleDTO
+    {
+        public int Id { get; set; }
+        public Day DayOfWeek { get; set; }
+        public List<ScheduleSlotDTO> times { get; set; }
+        public ActivationStatus AppointmentStatus { get; set; }
+    }
+    public class UpdateDoctorScheduleDTO
+    {
+        public int Id { get; set; }
+        public Day DayOfWeek { get; set; }
+        public List<UpdateScheduleSlotDTO> times { get; set; }
+        public ActivationStatus AppointmentStatus { get; set; }
+    }
+    public class ScheduleSlotDTO
+    {
+        public int Id { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+    public class UpdateScheduleSlotDTO
+    {
+        public int Id { get; set; }
+        public string Time { get; set; }
+    }
+}
