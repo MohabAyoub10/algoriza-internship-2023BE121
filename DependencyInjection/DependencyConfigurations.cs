c707c57b2749636738ef41a9a041f25ccd35b5be
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-07 21:07:04 +0200

    initial dependency configuration file to register the new services

---
+++ DependencyInjection/DependencyConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using Core.Domain;
+using Core.Repository;
+using Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+using Services;
+
+namespace DependencyInjection
+{
+    public class DependencyConfigurations
+    {
+        public static IServiceCollection DependenciesConfigurations(IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole>(
+                options => options.Password.RequireDigit = true
+                ).
+                AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IAppointmentTimeService, AppointmentTimeService>();
+            services.AddTransient<IAppointmentService, AppointmentService>();
+            return services;
+
+        }
+    }
+}
