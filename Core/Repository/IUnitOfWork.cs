8d4c71c42039e19ebea57e2d5e521d9b69200649
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-07 21:01:09 +0200

    Added the core class to manage and handle all repositories through it

---
+++ Core/Repository/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAppointmentRepository Appointments { get; }
+        IBaseRepository<AppointmentTimes> TimeSlots { get; }
+        IDoctorRepository Doctors { get; }
+
+        void Save();
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using Core.Domain;
+using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        public IDoctorRepository Doctors { get; private set; }
+        public IAppointmentRepository Appointments { get; private set; }
+        public IBaseRepository<AppointmentTimes> TimeSlots { get; private set; }
+
+
+
+        private readonly ApplicationDbContext _context;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+
+            Appointments = new AppointmentRepository(_context);
+            TimeSlots = new BaseRepository<AppointmentTimes>(_context);
+        }
+
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
