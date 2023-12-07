0ce2dc85530b14e6fc023e6ebaccd23d6e581ad9
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-07 20:34:35 +0200

    Changed the datatype for UserId , Added primary key Id

--- Core/Domain/Doctor.cs
+++ Core/Domain/Doctor.cs
@@ -11,7 +11,8 @@ namespace Core.Domain
 {
     public class Doctor
     {
-        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string UserId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
 
         public int SpecialtiesId { get; set; }
