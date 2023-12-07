ce4f8f6ecaf5bc8bf2268e5abccab467c076a8f6
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-07 21:05:59 +0200

    Created Doctor repository and services files

---
+++ Core/Repository/IDoctorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain;
+namespace Core.Repository
+{
+    public interface IDoctorRepository: IBaseRepository<Doctor>
+    {
+    }
+}
