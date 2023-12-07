2976ba3d3a41d9beb2821810aa80c36cb01c7259
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-07 20:28:40 +0200

    Modifies the Book Appointments table

--- Core/Domain/BookedAppointments.cs
+++ Core/Domain/BookedAppointments.cs
@@ -1,21 +1,27 @@
-﻿using Core.Utilities;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities;
 namespace Core.Domain
 {
     public class BookedAppointments
     {
         public int Id { get; set; }
         public string PatientId { get; set; }
-        public ApplicationUser Patient { get; set; }
-        public string DoctorId { get; set; }
-        public ApplicationUser Doctor { get; set; }
-        public DateTime Date { get; set; }
-        public int AppointmentTimesId { get; set; }
-        public AppointmentTimes AppointmentTimes { get; set; }
-        public BookingStatus bookingStatus { get; set; }
-        public DateTime CreatedAt  = DateTime.Now;
+        public int DoctorId { get; set; }
+        public int AppointmentTimeId { get; set; }
+        public BookingStatus AppointmentStatus { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public DateTime CreatedAt =  DateTime.Now;
+
         public int? PromoCodeId { get; set; }
-        public PromoCodes? PromoCode { get; set; }
+        public ApplicationUser Patient { get; set; }
+        public Doctor Doctor { get; set; }
+        public AppointmentTimes AppointmentTime { get; set; }
+        public PromoCodes PromoCode { get; set; }
+
 
 
     }
