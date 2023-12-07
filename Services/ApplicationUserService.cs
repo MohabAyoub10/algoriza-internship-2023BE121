ab822b9670a64db2e0c557e8b98cc23314d3dc4d
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-07 21:08:20 +0200

    Application user services file

---
+++ Services/ApplicationUserService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Repository;
+using Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    internal class ApplicationUserService : IApplicationUserService
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+        public ApplicationUserService(IUnitOfWork UnitOfWork) 
+        { 
+            _unitOfWork = UnitOfWork;
+        }
+
+    }
+}
