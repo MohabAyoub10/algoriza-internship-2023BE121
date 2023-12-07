827e8695b632ee66bb4a6213a523d922577954d5
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-07 21:09:16 +0200

    Added dependency configurations, JSON settings file, and database context to the application startup

--- Vezeeta/Program.cs
+++ Vezeeta/Program.cs
@@ -1,7 +1,15 @@
+using DependencyInjection;
+
+using Microsoft.EntityFrameworkCore;
+using Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+DependencyConfigurations.DependenciesConfigurations(builder.Services);
 
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Vezeeta")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
