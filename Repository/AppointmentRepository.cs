9e838e8bfc540ee9cc3c9ccc336e2679180e6fae
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-07 20:57:11 +0200

    Created an Appointment repository to manage the CRUD Operations for doctor appointments

---
+++ Repository/AppointmentRepository.cs
@@ -0,0 +1,65 @@
+﻿using Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class AppointmentRepository : IAppointmentRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public AppointmentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(DoctorSchedule entity)
+        {
+            _context.DoctorSchedules.Add(entity);
+        }
+        public void AddRange(IEnumerable<DoctorSchedule> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(DoctorSchedule entity)
+        {
+            _context.DoctorSchedules.Remove(entity);
+        }
+
+        public IEnumerable<DoctorSchedule> GetAll()
+        {
+            return _context.DoctorSchedules.ToList();
+        }
+
+        public DoctorSchedule GetById(int id)
+        {
+            return _context.DoctorSchedules.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(DoctorSchedule entity)
+        {
+            _context.DoctorSchedules.Update(entity);
+        }   
+
+        public IEnumerable<DoctorSchedule> GetAllById(int Id)
+        {
+            return _context.DoctorSchedules.Include(t => t.AppointmentTimes).Where(x => x.DoctorId == Id).ToList();
+
+        }
+
+        public void UpdateRange(IEnumerable<DoctorSchedule> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(IEnumerable<DoctorSchedule> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
