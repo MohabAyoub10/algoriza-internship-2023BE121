530973fb5d95b4b9f2381a34fd002427b13a04ad
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-09 13:22:02 +0200

    add change some fields to the nullable status

--- Core/Domain/ApplicationUser.cs
+++ Core/Domain/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,13 +18,14 @@ namespace Core.Domain
         [Required]
         public Gender Gender { get; set; }
         public UserType UserType { get; set; }
-        public string Address { get; set; }
-        [Required(ErrorMessage = "National Id is required.")]
+
+        public string? Address { get; set; }
         [StringLength(14, MinimumLength = 14, ErrorMessage = "National Id must be 14 digits.")]
-        public string NationalId { get; set; }
+
+        public string? NationalId { get; set; }
         [Required(ErrorMessage = "Date of Birth is required.")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
-        public string Image { get; set; }
+        public string? Image { get; set; }
     }
 }
