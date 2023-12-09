a2c249baa4019a16ac50bb48a20e1af9cbcad5d4
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-09 13:23:57 +0200

    add doctor dto to add new doctor

---
+++ Core/DTO/DoctorDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTO
+{
+    public class DoctorDTO
+    {
+        [Required(ErrorMessage = "Speciality is required.")]
+        public string Speciality { get; set; }
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0, 10000)]
+        public float Price { get; set; }
+    }
+}
