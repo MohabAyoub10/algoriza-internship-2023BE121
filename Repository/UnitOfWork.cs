9c3b4731abe0d0a3fa826894f5d67fb555c4ee34
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-09 14:29:20 +0200

    add user and role manager

--- Repository/UnitOfWork.cs
+++ Repository/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Domain;
 using Core.Repository;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +15,22 @@ namespace Repository
         public IAppointmentRepository Appointments { get; private set; }
         public IBaseRepository<AppointmentTimes> TimeSlots { get; private set; }
 
+        public IApplicationUserRepository ApplicationUsers { get; private set; }
+
 
 
         private readonly ApplicationDbContext _context;
-        public UnitOfWork(ApplicationDbContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UnitOfWork(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
 
             Appointments = new AppointmentRepository(_context);
             TimeSlots = new BaseRepository<AppointmentTimes>(_context);
+            ApplicationUsers = new ApplicationUserRepository(_context, _userManager,_roleManager);
         }
 
 
