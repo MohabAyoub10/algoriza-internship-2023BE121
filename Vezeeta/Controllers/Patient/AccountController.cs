c607283baec852b36dc6abbed84599a939fa4291
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-09 14:19:00 +0200

    Create an Account Controller for patients to register and login

--- Vezeeta/Controllers/Patient/AccountController.cs
+++ Vezeeta/Controllers/Patient/AccountController.cs
@@ -1,5 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Services;
+using Core.DTO;
+using Core.Repository;
+using Core.Domain;
+using Core.Services;
 
 namespace Vezeeta.Controllers.Patient
 {
@@ -7,5 +14,43 @@ namespace Vezeeta.Controllers.Patient
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IApplicationUserService _applicationUserService;
+
+
+        public AccountController(IUnitOfWork unitOfWork, IApplicationUserService applicationUserService)
+        {
+            _unitOfWork = unitOfWork;
+            _applicationUserService = applicationUserService;
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromForm] UserRegistrationDTO register)
+        {
+            try
+            {
+                return await _applicationUserService.AddUser(register);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            try
+            {
+                return await _applicationUserService.LogIn(username, password);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
