8a1a7179932ff970006d8b828ff62774ecb1547b
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-10 11:33:09 +0200

    added an new method

--- Core/Repository/IDoctorRepository.cs
+++ Core/Repository/IDoctorRepository.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Core.Domain;
+using Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Core.Repository
 {
-    public interface IDoctorRepository: IBaseRepository<Doctor>
+    public interface IDoctorRepository
     {
+        IActionResult AllDoctorsInfoWithAppointment(int Page, int PageSize, Func<DoctorDTO, bool> criteria = null);
     }
 }
