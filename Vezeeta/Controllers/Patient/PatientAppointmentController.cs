8a6fdf55265ff5a94771254172a8f4f77da81cfc
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-12-10 11:22:08 +0200

    new endpoint return all doctors

--- Vezeeta/Controllers/Patient/PatientAppointmentController.cs
+++ Vezeeta/Controllers/Patient/PatientAppointmentController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using System;
+using Core.Services;    
 namespace Vezeeta.Controllers.Patient
 {
     [Route("api/appointment")]
     [ApiController]
     public class PatientAppointmentController : ControllerBase
     {
-        
+
+        private readonly IDoctorServices _doctorServices;
+
+        public PatientAppointmentController(IDoctorServices doctorServices)
+        {
+            _doctorServices = doctorServices;
+        }
+
+        [HttpGet("Doctors")]
+        public IActionResult AllDoctorsInfoWithAppointments(int Page, int PageSize, string? search)
+        {
+           
+            return _doctorServices.AllDoctorsInfoWithAppointments(Page, PageSize, search);
+        }
+
     }
 }
